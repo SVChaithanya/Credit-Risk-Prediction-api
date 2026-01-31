56e0ee8c836e43f6f6195f309e5e368efabca345
Surya <surya334r@gmail.com>
2026-01-31 17:19:13 +0530

    Implement database connection function
    Add a function to establish a PostgreSQL connection

--- postgreSQL/db.py
+++ postgreSQL/db.py
@@ -1,1 +1,14 @@
+import psycopg2
+def get_connenction():
+    try:
+        conn = psycopg2.connect(
+            host='localhost',
+            database='user_database',
+            user='postgres',
+            password='user_password',
+            port='port_number'
+        )
+        return conn
+    except Exception as e:
+        print("database error",e)    
 
